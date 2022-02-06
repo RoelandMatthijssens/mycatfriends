291aa25ecc352550835039de4870e35f59ba80c5
RoelandMatthijssens <fulgens.ailurus@gmail.com>
2022-02-06 18:14:53 +0100

    Add persistence of like counters

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
-import Avatar from './Avatar'
-import Counter from './Counter'
+import Avatar from "./Avatar";
+import Counter from "./Counter";
 
-function Card({user}) {
+function Card({ user }) {
   return (
     <div className="Card flex flex-column bg-light-green dib br3 pa3 ma2 bw2 shadow-5 tc hide-child cover">
-      <div className='self-end'>
-        <Counter />
+      <div className="self-end">
+        <Counter id={`cat-${user.email}`} />
       </div>
       <Avatar name={user.name} />
-      <h1 className='w5'>{user.name}</h1>
+      <h1 className="w5">{user.name}</h1>
       <p>{user.email}</p>
     </div>
   );
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,16 +1,39 @@
-import {useState} from 'react'
-import { AiOutlineHeart } from 'react-icons/ai';
+import { useState, useEffect } from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+function Counter({ id }) {
+  const [count, setCount] = useState(0);
 
-function Counter() {
-  const [count, setCount] = useState(0)
+  const store_likes = async () => {
+    try {
+      await AsyncStorage.setItem(id, JSON.stringify(count));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+  const retrieve_likes = async () => {
+    try {
+      const stored_likes = await AsyncStorage.getItem(id);
+      if (stored_likes !== null) {
+        setCount(JSON.parse(stored_likes));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    retrieve_likes();
+  }, []);
+  useEffect(() => {
+    store_likes(count);
+  }, [count]);
 
   return (
-    <div className="Counter flex mr2" onClick={() => setCount(count+1)}>
-      <div className='f3 mt2'>
-        {count}
-      </div>
-      <AiOutlineHeart className='f1'/>
+    <div className="Counter flex mr2" onClick={() => setCount(count + 1)}>
+      <div className="f3 mt2">{count}</div>
+      <AiOutlineHeart className="f1" />
     </div>
   );
 }
